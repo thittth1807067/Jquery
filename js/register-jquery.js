97b38328f9a5e6e523b7563b7d9ccf127a7131ab
thi <thittth1807067@fpt.edu.vn>
2018-11-22 12:40:59 +0700

    upload login và creat song

--- js/register-jquery.js
+++ js/register-jquery.js
@@ -1,9 +1,11 @@
+
+
 $('#register-form').validate({
     rules: {
         firstName: {
             required: true,
             minlength: 2,
-            maxlength: 15
+            maxlength: 15,
         },
         lastName: {
             required: true,
@@ -18,6 +20,15 @@ $('#register-form').validate({
             required: true,
             minlength: 2,
             maxlength: 15
+        },
+        gender: {
+            required: true,
+        },
+        address: {
+            required: true,
+        },
+        avatar:{
+            required:true,
         },
         'confirm-password': {
             equalTo: '[name="password"]'
@@ -43,8 +54,70 @@ $('#register-form').validate({
             minlength: 'Password quá ngắn, vui lòng nhập ít nhất {0} ký tự',
             maxlength: 'Password quá dài, vui lòng nhập nhiều nhất {0} ký tự',
         },
+        phone: {
+            required: 'Vui lòng nhập số điện thoại của bạn.',
+            minlength: 'số điện thoại không hợp lệ, vui lòng nhập ít nhất {0} ký tự',
+            maxlength: 'số điện thoại không hợp lệ, vui lòng nhập nhiều nhất {0} ký tự',
+        },
+        gender: {
+            required: 'Vui lòng chọn giới tính',
+        },
+        address: {
+            required: 'Vui lòng nhập địa chỉ',
+        },
+        avatar:{
+            required:'vui lòng nhập avatar',
+        },
         'confirm-password': {
             equalTo: 'Password và confirm không giống nhao.'
         }
+    },
+    submitHandler: function (form,event) {
+        event.preventDefault();
+        var senderObject = {
+            firstName: $(form["firstName"]).val(),
+            lastName: $(form["lastName"]).val(),
+            password: $(form["password"]).val(),
+            address: $(form["address"]).val(),
+            phone: $(form["phone"]).val(),
+            gender: $(form["gender"]).val(),
+            email: $(form["email"]).val(),
+            avatar: $(form["avatar"]).val(),
+            birthday: formatDate($(form["birthday"]).val())
+        };
+        //alert(JSON.stringify(senderObject));
+        $.ajax({
+            type: "POST",
+            url: REGISTER_API,
+            contentType: 'application/json; charset=utf-8',
+            data: JSON.stringify(senderObject),
+            success: function (data, textStatus, jqXHR) {
+                console.log('success');
+                console.log(data);
+                console.log('-----');
+                console.log(data.responseText);
+                console.log('-----');
+                console.log(textStatus);
+                console.log('-----');
+                console.log(jqXHR);
+            },
+            error: function (jqXHR, textStatus, errorThrown) {
+                console.log('error');
+                // if (Object.keys(jqXHR.responseJSON.error).length > 0) {
+                //     $('#summary').text(please fix ${Object.keys(jqXHR.responseJSON.error).length} below!);
+                //     validater.showErrors(jqXHR.responseJSON.error);
+                // }
+            }
+        });
+        return false;
     }
 });
+function formatDate(date) {
+    var d = new Date (date),
+        month = '' + (d.getMonth() +1),
+        day = '' + d.getDate(),
+        year = d.getFullYear();
+    if (month.length <2) month = '0' + month;
+    if (day.length <2) day = '0' + day;
+    return [year , month, day].join('-');
+}
--- js/save-song.js
+++ js/save-song.js
@@ -1,54 +1,27 @@
-document.addEventListener('DOMContentLoaded', function () {
-    var songForm = document.forms['song-form'];
-    if (songForm == null || songForm['btn-submit'] == null) {
-        alert('Vui lòng thử lại!');
-        return;
-    }
-    songForm['btn-submit'].onclick = function () {
-        var txtName = songForm['name'];
-        var txtDescription = songForm['description'];
-        var txtSinger = songForm['singer'];
-        var txtAuthor = songForm['author'];
-        var txtThumbnail = songForm['thumbnail'];
-        var txtLink = songForm['link'];
-        if (txtName == null
-            || txtDescription == null
-            || txtSinger == null
-            || txtAuthor == null
-            || txtThumbnail == null
-            || txtLink == null) {
-            alert('Vui lòng thử lại!');
-            return;
+submitHandler: function (form,event) {
+    event.preventDefault();
+    var senderObject = {
+        name: $(form["name"]).val(),
+        singer: $(form["singer"]).val(),
+        description: $(form["description"]).val(),
+        author: $(form["author"]).val(),
+        thumbnail: $(form["thumbnail"]).val(),
+        link: $(form["link"]).val(),
+    };
+    //alert(JSON.stringify(senderObject));
+    $.ajax({
+        type: "POST",
+        url: CREATE_SONG_API,
+        contentType: 'application/json; charset=utf-8',
+        data: JSON.stringify(senderObject),
+        headers: {'authorization': 'Basic ' + localStorage.getItem('token')},
+        success: function (data, textStatus, jqXHR) {
+            console.log('success');
+            alert('dang ki thanh cong');
+        },
+        error: function (jqXHR, textStatus, errorThrown) {
+            console.log('error');
         }
-
-        var jsSong = {
-            name: txtName.value,
-            description: txtDescription.value,
-            singer: txtSinger.value,
-            author: txtAuthor.value,
-            thumbnail: txtThumbnail.value,
-            link: txtLink.value
-        }
-        createSong(jsSong);
-    }
-});
-
-function createSong(jsSong) {
-    var xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = function () {
-        if (this.readyState == 4 && this.status == 201) {
-            var song = JSON.parse(this.responseText);
-            alert(`Lưu thành công bài hát ${song.name}`);
-        } else if (this.readyState == 4 && (this.status == 401 || this.status == 403)) {
-            alert('This action required logged in to continue!')
-            window.location.href = "login.html";
-        }
-    }
-    xhr.open('POST', CREATE_SONG_API, true);
-    xhr.setRequestHeader("Content-type", "application/json");
-    xhr.setRequestHeader("Authorization", "Basic " + localStorage.getItem('token-key'));
-    xhr.send(JSON.stringify(jsSong));
+    });
+    return false;
 }
-
-
-
